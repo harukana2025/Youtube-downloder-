cac83fe256afd52b332112cfcd1f8eed94f0441e
harukana2025 <sbhkrkhk@gmail.com>
2026-05-06 13:08:19 +0900

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,9 @@
+@echo off
+title YouTube???????
+echo ========================================
+echo    YouTube??????????...
+echo ========================================
+cd /d C:\YouTubeDownloader
+start http://localhost:5000
+python app.py
+pause
